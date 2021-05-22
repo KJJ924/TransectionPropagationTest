e6ad09bd736c950825984238a7da84072c055542
jaejoon <dkansk924@naver.com>
2021-05-23 00:47:02 +0900

    [Add] board Entity

---
+++ src/main/java/study/jpatransactional/pessimisticlock/entity/Board.java
@@ -0,0 +1,31 @@
+package study.jpatransactional.pessimisticlock.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * @author dkansk924@naver.com
+ * @since 2021/05/23
+ */
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+
+    private Long boardCount;
+}
